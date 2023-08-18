9484c85713701abde27894e14fcae3be5553aef7
Sumit <info.framestudio21@gmail.com>
2023-08-18 21:32:11 +0530

    update in hero Recipe Section Commponet

--- src/component/herorecipe.js
+++ src/component/herorecipe.js
@@ -1,9 +1,14 @@
 import Link from "next/link"
 import Image from "next/image"
 
-import { FaBeer } from 'react-icons/fa';
+import { FaCocktail, FaGlassMartiniAlt } from 'react-icons/fa';
+import { SiCakephp } from 'react-icons/si';
+import { BiSolidUser } from 'react-icons/bi';
+import { GiChickenOven, GiFullPizza, GiCupcake, GiNoodles } from 'react-icons/gi';
 
 import style from "./styles/herorecipe.module.css"
+
+import img from "../image/img1.jpg"
 
 export default function herorecipe() {
   return (
@@ -17,13 +22,78 @@ export default function herorecipe() {
         </div>
         <div className={style.herorecipeBottom}>
             <div className={style.herorecipeBottomLeft}>
+
                 <div className={style.recipeIconDiv}>
-                    <div className={style.recipeIcon}></div>
-                    <div className={style.recipeIconTitle}></div>
+                    <div className={style.recipeIcon}>
+                            <FaCocktail className={style.iconBody}/>
+                    </div>
+                    <div className={style.recipeIconTitle}>Cocktail</div>
+                </div>
+
+                <div className={style.recipeIconDiv}>
+                    <div className={style.recipeIcon}>
+                            <FaGlassMartiniAlt className={style.iconBody}/>
+                    </div>
+                    <div className={style.recipeIconTitle}>mocktail</div>
+                </div>
+
+                <div className={style.recipeIconDiv}>
+                    <div className={style.recipeIcon}>
+                            <GiChickenOven className={style.iconBody}/>
+                    </div>
+                    <div className={style.recipeIconTitle}>Chicken</div>
+                </div>
+
+                <div className={style.recipeIconDiv}>
+                    <div className={style.recipeIcon}>
+                            <GiFullPizza className={style.iconBody}/>
+                    </div>
+                    <div className={style.recipeIconTitle}>Pizza</div>
+                </div>
+
+                <div className={style.recipeIconDiv}>
+                    <div className={style.recipeIcon}>
+                            <GiCupcake className={style.iconBody}/>
+                    </div>
+                    <div className={style.recipeIconTitle}>Dessert</div>
+                </div>
+
+                <div className={style.recipeIconDiv}>
+                    <div className={style.recipeIcon}>
+                            <SiCakephp className={style.iconBody}/>
+                    </div>
+                    <div className={style.recipeIconTitle}>Cake</div>
+                </div>
+
+                <div className={style.recipeIconDiv}>
+                    <div className={style.recipeIcon}>
+                            <GiNoodles className={style.iconBody}/>
+                    </div>
+                    <div className={style.recipeIconTitle}>Noodles</div>
+                </div>
+
+            </div>
+            <div className={style.herorecipeBottomMiddle}>
+                <Image width={0} height={0} className={style.image} src={img}/>
+            </div>
+            <div className={style.herorecipeBottomRight}>
+                <div className={style.recipeDetailDiv}>
+                    <div className={style.recipeDetailDivTop}>
+                        Russian Salad
+                    </div>
+                    <div className={style.recipeDetailDivBottom}>
+                        <div className={style.recipeDetailDivBottomLeft}>
+                            <div className={style.recipeDetailDivBottomLeftTitle}></div>
+                            <div className={style.recipeDetailDivBottomLeftIcon}>
+                                <BiSolidUser className={style.icon}/>
+                                <BiSolidUser className={style.icon}/>
+                            </div>
+                        </div>
+                        <div className={style.recipeDetailDivBottomMiddle}></div>
+                        <div className={style.recipeDetailDivBottomRight}></div>
+                    </div>
                 </div>
             </div>
-            <div className={style.herorecipeBottomMiddle}></div>
-            <div className={style.herorecipeBottomRight}></div>
         </div>
     </div>
     </>
