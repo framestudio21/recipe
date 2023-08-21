714d487ade5c57302b10f40e6baf63d014f579b8
Gourav <duarygourav@gmail.com>
2023-08-21 21:28:08 +0530

    Recipecontainer created in trendingrecipies file

--- src/component/trendingRecipes.jsx
+++ src/component/trendingRecipes.jsx
@@ -5,6 +5,43 @@ import { BiSolidUser } from 'react-icons/bi';
 import img from '../image/img1.jpg'
 import styles from './styles/trendingrecipes.module.css'
 
+export function Recipecontainer () {
+    return(
+        <div className={styles.container}>
+                <Image
+                    width={250}
+                    height={160}
+                    objectFit="cover"
+                    src={img}
+                    alt="img"
+                    className={styles.img}
+                />
+                <div className={styles.containerDetails}>
+                <div className={styles.tag}>caribban</div>
+                <div className={styles.recipeName}>Cuban Ropa Vieja</div>
+                <div className={styles.nameBar}></div>
+                <div className={styles.detailBottom}>
+                    <div className={styles.serving}>
+                        <div className={styles.servingTitle}>Serving</div>
+                        <div className={styles.servingPerson}>
+                            <BiSolidUser className={styles.servingPersonIcon}/>
+                            <BiSolidUser className={styles.servingPersonIcon}/>
+                        </div>
+                    </div>
+                    <div className={styles.bottomBar}></div>
+                    <div className={styles.recipeTime}>
+                        <div className={styles.timeTitle}>Cooking Time</div>
+                        <div className={styles.timeIcon}>
+                            <PiClockCountdownFill className={styles.clockIcon}/>
+                            <div className={styles.time}>1h30min</div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+    )
+}
+
 export default function trendingRecipes() {
   return (
     <div className={styles.trendingRecipes}>
@@ -16,139 +53,12 @@ export default function trendingRecipes() {
             </div>
         </div>
         <div className={styles.bottom}>
-
-            <div className={styles.container}>
-                <Image
-                    width={250}
-                    height={160}
-                    objectFit="cover"
-                    src={img}
-                    alt="img"
-                    className={styles.img}
-                />
-                <div className={styles.containerDetails}>
-                <div className={styles.tag}>caribban</div>
-                <div className={styles.recipeName}>Cuban Ropa Vieja</div>
-                <div className={styles.nameBar}></div>
-                <div className={styles.detailBottom}>
-                    <div className={styles.serving}>
-                        <div className={styles.servingTitle}>Serving</div>
-                        <div className={styles.servingPerson}>
-                            <BiSolidUser className={styles.servingPersonIcon}/>
-                            <BiSolidUser className={styles.servingPersonIcon}/>
-                        </div>
-                    </div>
-                    <div className={styles.bottomBar}></div>
-                    <div className={styles.recipeTime}>
-                        <div className={styles.timeTitle}>Cooking Time</div>
-                        <div className={styles.timeIcon}>
-                            <PiClockCountdownFill className={styles.clockIcon}/>
-                            <div className={styles.time}>1h30min</div>
-                        </div>
-                    </div>
-                </div>
-                </div>
-            </div>
-
-            <div className={styles.container}>
-                <Image
-                    width={250}
-                    height={160}
-                    objectFit="cover"
-                    src={img}
-                    alt="img"
-                    className={styles.img}
-                />
-                <div className={styles.containerDetails}>
-                <div className={styles.tag}>caribban</div>
-                <div className={styles.recipeName}>Cuban Ropa Vieja</div>
-                <div className={styles.nameBar}></div>
-                <div className={styles.detailBottom}>
-                    <div className={styles.serving}>
-                        <div className={styles.servingTitle}>Serving</div>
-                        <div className={styles.servingPerson}>
-                            <BiSolidUser className={styles.servingPersonIcon}/>
-                            <BiSolidUser className={styles.servingPersonIcon}/>
-                        </div>
-                    </div>
-                    <div className={styles.bottomBar}></div>
-                    <div className={styles.recipeTime}>
-                        <div className={styles.timeTitle}>Cooking Time</div>
-                        <div className={styles.timeIcon}>
-                            <PiClockCountdownFill className={styles.clockIcon}/>
-                            <div className={styles.time}>1h30min</div>
-                        </div>
-                    </div>
-                </div>
-                </div>
-            </div>
-
-            <div className={styles.container}>
-                <Image
-                    width={250}
-                    height={160}
-                    objectFit="cover"
-                    src={img}
-                    alt="img"
-                    className={styles.img}
-                />
-                <div className={styles.containerDetails}>
-                <div className={styles.tag}>caribban</div>
-                <div className={styles.recipeName}>Cuban Ropa Vieja</div>
-                <div className={styles.nameBar}></div>
-                <div className={styles.detailBottom}>
-                    <div className={styles.serving}>
-                        <div className={styles.servingTitle}>Serving</div>
-                        <div className={styles.servingPerson}>
-                            <BiSolidUser className={styles.servingPersonIcon}/>
-                            <BiSolidUser className={styles.servingPersonIcon}/>
-                        </div>
-                    </div>
-                    <div className={styles.bottomBar}></div>
-                    <div className={styles.recipeTime}>
-                        <div className={styles.timeTitle}>Cooking Time</div>
-                        <div className={styles.timeIcon}>
-                            <PiClockCountdownFill className={styles.clockIcon}/>
-                            <div className={styles.time}>1h30min</div>
-                        </div>
-                    </div>
-                </div>
-                </div>
-            </div>
-
-            <div className={styles.container}>
-                <Image
-                    width={250}
-                    height={160}
-                    objectFit="cover"
-                    src={img}
-                    alt="img"
-                    className={styles.img}
-                />
-                <div className={styles.containerDetails}>
-                <div className={styles.tag}>caribban</div>
-                <div className={styles.recipeName}>Cuban Ropa Vieja</div>
-                <div className={styles.nameBar}></div>
-                <div className={styles.detailBottom}>
-                    <div className={styles.serving}>
-                        <div className={styles.servingTitle}>Serving</div>
-                        <div className={styles.servingPerson}>
-                            <BiSolidUser className={styles.servingPersonIcon}/>
-                            <BiSolidUser className={styles.servingPersonIcon}/>
-                        </div>
-                    </div>
-                    <div className={styles.bottomBar}></div>
-                    <div className={styles.recipeTime}>
-                        <div className={styles.timeTitle}>Cooking Time</div>
-                        <div className={styles.timeIcon}>
-                            <PiClockCountdownFill className={styles.clockIcon}/>
-                            <div className={styles.time}>1h30min</div>
-                        </div>
-                    </div>
-                </div>
-                </div>
-            </div>
-
+            <Recipecontainer/>
+            <Recipecontainer/>
+            <Recipecontainer/>
+            <Recipecontainer/>          
+            <Recipecontainer/>
+            <Recipecontainer/>
         </div>
     </div>
   )
